cff5cb68d7296e3826241db70e92cfca561b6532
dscornaienchi <Dscorni32@gmail.com>
2023-10-23 18:38:53 -0500

    added logic to quit application

--- index.js
+++ index.js
@@ -1,38 +1,41 @@
 import inquirer from 'inquirer';
 import { getAllDepartments, getAllRoles, getAllEmployees } from './db/queries.js';
 
-
 async function mainMenu() {
+  while (true) { // Use an infinite loop
     const { action } = await inquirer.prompt({
-        type: 'list',
-        name: 'action',
-        message: 'Select an action:',
-        choices: [
-            'View all departments',
-            'View all roles',
-            'View all employees',
-            'Add a department',
-            'Add a role',
-            'Add an employee',
-            'Update an employee role',
-            'Quit'
-        ],
+      type: 'list',
+      name: 'action',
+      message: 'Select an action:',
+      choices: [
+        'View all departments',
+        'View all roles',
+        'View all employees',
+        'Add a department',
+        'Add a role',
+        'Add an employee',
+        'Update an employee role',
+        'Quit',
+      ],
     });
 
     switch (action) {
-        case 'View all departments':
-            const departments = await getAllDepartments();
-            console.table(departments);
-            break;
-        case 'View all roles':
-            const roles = await getAllRoles();
-            console.table(roles);
-            break;
-        case 'View all employees':
-            const employees = await getAllEmployees();
-            console.table(employees);
-            break;
+      case 'View all departments':
+        const departments = await getAllDepartments();
+        console.table(departments);
+        break;
+      case 'View all roles':
+        const roles = await getAllRoles();
+        console.table(roles);
+        break;
+      case 'View all employees':
+        const employees = await getAllEmployees();
+        console.table(employees);
+        break;
+      case 'Quit':
+        return; // Use 'return' to exit the function and return to the terminal
     }
+  }
 }
 
 mainMenu();
